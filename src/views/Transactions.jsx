d3e7c2b7424bb112c45a2b496463f7677029acf6
Emanuel Lourenço <dodgy6@gmail.com>
2021-01-03 17:18:08 +0000

    add table in transactions view component

--- src/views/Transactions.jsx
+++ src/views/Transactions.jsx
@@ -1,13 +1,86 @@
 import React from "react";
-import { Card, Row, Col } from "antd";
+import { Row, Col } from "antd";
+import { Table, Tag, Space } from "antd";
+
 import MainLayout from "../components/layout/MainLayout";
 
 function Transactions() {
+  const columns = [
+    {
+      title: "Date",
+      dataIndex: "date",
+      key: "date",
+      sorter: (a, b) => a.age - b.age,
+      render: (text) => <span>{text}</span>,
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      sorter: (a, b) => a.age - b.age,
+      render: (text) => <span>{text}</span>,
+    },
+    {
+      title: "Tags",
+      key: "tags",
+      dataIndex: "tags",
+      render: (tags) => (
+        <>
+          {tags.map((tag) => {
+            let color = tag.length > 5 ? "geekblue" : "green";
+            if (tag === "loser") {
+              color = "volcano";
+            }
+            return (
+              <Tag color={color} key={tag}>
+                {tag.toUpperCase()}
+              </Tag>
+            );
+          })}
+        </>
+      ),
+    },
+    {
+      title: "Value",
+      dataIndex: "value",
+      key: "value",
+      sorter: (a, b) => a.age - b.age,
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (text, record) => <Space size="middle">Edit | Delete</Space>,
+    },
+  ];
+
+  const data = [
+    {
+      key: "1",
+      name: "John Brown",
+      age: 32,
+      value: "80,00€",
+      tags: ["nice", "developer"],
+    },
+    {
+      key: "2",
+      name: "Jim Green",
+      age: 42,
+      value: "20,00€",
+      tags: ["loser"],
+    },
+    {
+      key: "3",
+      name: "Joe Black",
+      age: 32,
+      value: "-20,00€",
+      tags: ["cool", "teacher"],
+    },
+  ];
   return (
     <MainLayout>
       <Row>
-        <Col sm={24} md={12} lg={8}>
-          <Card title="Income">100,00€</Card>
+        <Col sm={24}>
+          <Table columns={columns} dataSource={data} />
         </Col>
       </Row>
     </MainLayout>
