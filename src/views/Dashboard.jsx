26917d6a5b24c16f8a2520848c71e2a1794bd3b6
Emanuel Lourenço <dodgy6@gmail.com>
2021-01-04 17:57:25 +0000

    change Dashboard chart data and layout

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -10,103 +10,45 @@ function Login() {
     }),
     []
   );
+
   const axes = React.useMemo(
     () => [
-      { primary: true, position: "bottom", type: "time" },
+      { primary: true, position: "bottom", type: "ordinal" },
       { position: "left", type: "linear", stacked: false },
     ],
     []
   );
+
   const data = React.useMemo(
     () => [
       {
         label: "Series 1",
         datums: [
-          {
-            x: new Date("2020-03-18T11:00:00.000Z"),
-            y: 60,
-          },
-          {
-            x: new Date("2020-03-18T11:30:00.000Z"),
-            y: 23,
-          },
-          {
-            x: new Date("2020-03-18T12:00:00.000Z"),
-            y: 65,
-          },
-          {
-            x: new Date("2020-03-18T12:30:00.000Z"),
-            y: 84,
-          },
-          {
-            x: new Date("2020-03-18T13:00:00.000Z"),
-            y: 87,
-          },
-          {
-            x: new Date("2020-03-18T13:30:00.000Z"),
-            y: 84,
-          },
-          {
-            x: new Date("2020-03-18T14:00:00.000Z"),
-            y: 96,
-          },
-          {
-            x: new Date("2020-03-18T14:30:00.000Z"),
-            y: 88,
-          },
-          {
-            x: new Date("2020-03-18T15:00:00.000Z"),
-            y: 63,
-          },
-          {
-            x: new Date("2020-03-18T15:30:00.000Z"),
-            y: 60,
-          },
+          { x: "Jan", y: 60 },
+          { x: "Feb", y: 23 },
+          { x: "Mar", y: 65 },
+          { x: "Apr", y: 84 },
+          { x: "May", y: 87 },
+          { x: "Jun", y: 84 },
+          { x: "Jul", y: 96 },
+          { x: "Agu", y: 88 },
+          { x: "Sep", y: 63 },
+          { x: "Oct", y: 60 },
         ],
       },
       {
         label: "Series 2",
         datums: [
-          {
-            x: new Date("2020-03-18T11:00:00.000Z"),
-            y: 41,
-          },
-          {
-            x: new Date("2020-03-18T11:30:00.000Z"),
-            y: 15,
-          },
-          {
-            x: new Date("2020-03-18T12:00:00.000Z"),
-            y: 95,
-          },
-          {
-            x: new Date("2020-03-18T12:30:00.000Z"),
-            y: 96,
-          },
-          {
-            x: new Date("2020-03-18T13:00:00.000Z"),
-            y: 33,
-          },
-          {
-            x: new Date("2020-03-18T13:30:00.000Z"),
-            y: 96,
-          },
-          {
-            x: new Date("2020-03-18T14:00:00.000Z"),
-            y: 32,
-          },
-          {
-            x: new Date("2020-03-18T14:30:00.000Z"),
-            y: 49,
-          },
-          {
-            x: new Date("2020-03-18T15:00:00.000Z"),
-            y: 18,
-          },
-          {
-            x: new Date("2020-03-18T15:30:00.000Z"),
-            y: 69,
-          },
+          { x: "Jan", y: 41 },
+          { x: "Feb", y: 15 },
+          { x: "Mar", y: 95 },
+          { x: "Apr", y: 96 },
+          { x: "May", y: 33 },
+          { x: "Jun", y: 96 },
+          { x: "Jul", y: 32 },
+          { x: "Agu", y: 49 },
+          { x: "Sep", y: 18 },
+          { x: "Oct", y: 69 },
         ],
       },
     ],
@@ -141,7 +83,7 @@ function Login() {
           </Card>
         </Col>
       </Row>
-      <Row style={{ height: "500px" }}>
+      <Row style={{ height: "500px", padding: "50px 100px" }}>
         <Col span="24">
           <Chart data={data} series={series} axes={axes} tooltip />
         </Col>
