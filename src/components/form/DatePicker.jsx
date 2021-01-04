b6cea2deaefec2e445144ea26383db673c42bedb
Emanuel Lourenço <dodgy6@gmail.com>
2021-01-04 11:28:54 +0000

    add datepicker component

---
+++ src/components/form/DatePicker.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Row, Col, DatePicker } from "antd";
+import PropTypes from "prop-types";
+
+function DatePickerCol(props) {
+  const { cols, label, name, onChange } = props;
+
+  return (
+    <Col span={cols}>
+      <Row>
+        <Col span="24">
+          <span>{label}</span>
+        </Col>
+      </Row>
+      <Row>
+        <Col span="24">
+          <DatePicker
+            id={name}
+            name={name}
+            onChange={onChange}
+            style={{ width: "100%" }}
+          />
+        </Col>
+      </Row>
+    </Col>
+  );
+}
+
+DatePickerCol.defaultProps = {
+  cols: "24",
+  label: "",
+  name: "name",
+  onChange: () => {},
+};
+
+DatePickerCol.propTypes = {
+  cols: PropTypes.string,
+  label: PropTypes.string,
+  name: PropTypes.string,
+  onChange: PropTypes.func,
+};
+
+export default DatePickerCol;
