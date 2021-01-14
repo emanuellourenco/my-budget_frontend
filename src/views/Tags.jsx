350e6f2a7db1bec7007b02c3611a060ad9a250e9
Emanuel Lourenço <dodgy6@gmail.com>
2021-01-14 18:43:28 +0000

    add/change tag view functions and add documentation

--- src/views/Tags.jsx
+++ src/views/Tags.jsx
@@ -1,13 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useReducer } from "react";
 import axios from "axios";
 import { Row, Col, Card, Table, Tag, Button, Tooltip } from "antd";
 import MainLayout from "../components/layout/MainLayout";
 import { PlusOutlined } from "@ant-design/icons";
 import TagModal from "../components/modals/Tags";
 import ActionButtons from "../components/table/ActionButtons";
+import { TABLE_LIMIT } from "../variables/globalVariables";
 
 function Tags() {
-  const [data, setData] = useState([]);
+  const url = process.env.REACT_APP_URL;
+  const token = localStorage.getItem("token");
+  const [data, setData] = useReducer(
+    (state, newState) => ({ ...state, ...newState }),
+    {
+      tags: [],
+      total: 0,
+      variables: {
+        token,
+        offset: 0,
+        limit: TABLE_LIMIT,
+        orderBy: "name",
+        sortBy: "asc",
+      },
+    }
+  );
   const [modalOpen, setModalOpen] = useState(false);
   const [dataId, setdataId] = useState(null);
 
@@ -17,14 +33,14 @@ function Tags() {
       dataIndex: "name",
       key: "name",
       width: "200px",
-      sorter: (a, b) => a.age - b.age,
+      sorter: true,
       render: (text, record) => <Tag color={record.color}>{text}</Tag>,
     },
     {
       title: "Rule",
       dataIndex: "rule",
       key: "rule",
-      sorter: (a, b) => a.age - b.age,
+      sorter: true,
       render: (text) => <span>{text}</span>,
     },
     {
@@ -41,32 +57,81 @@ function Tags() {
     },
   ];
 
+  /**
+   * Set modal open to add or update tag
+   * @param {Int} id
+   */
   const handleChangeModal = (id) => {
     setModalOpen(true);
     setdataId(id);
   };
 
-  const removeData = (key) => {
-    const index = data.findIndex((item) => item.key === key);
-    const newData = [...data];
-    newData.splice(index, 1);
-    setData(newData);
-  };
-
-  useEffect(() => {
-    const url = process.env.REACT_APP_URL;
-    const token = localStorage.getItem("token");
-
+  /**
+   * Remove tag and update component data
+   * @param {Int} id
+   */
+  const removeData = (id) => {
     axios
-      .get(`${url}/tags?token=${token}`)
+      .delete(`${url}/tags/${id}`, { params: data.variables })
       .then(({ data }) => {
-        setData(data.tags);
+        setData({ tags: data.tags, total: data.total_count });
       })
       .catch((error) => {
         // handle error
         console.log(error);
       });
+  };
+
+  /**
+   * Update tags list in component didmount
+   */
+  useEffect(() => {
+    axios
+      .get(`${url}/tags`, { params: data.variables })
+      .then(({ data }) => {
+        setData({ tags: data.tags, total: data.total_count });
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  /**
+   * Update tags list
+   */
+  const getData = () => {
+    axios
+      .get(`${url}/tags`, { params: data.variables })
+      .then(({ data }) => {
+        setData({ tags: data.tags, total: data.total_count });
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      });
+  };
+
+  /**
+   * Function to update component variables and call "getData" function to update tags list
+   * @param {Object} pagination
+   * @param {Object} filters
+   * @param {Object} sorter
+   */
+  function onChange(pagination, filters, sorter) {
+    let updateVariables = data.variables;
+    const page = pagination.current;
+    const { field, order } = sorter;
+    const newOffset = TABLE_LIMIT * (page - 1);
+
+    updateVariables.offset = newOffset;
+    updateVariables.orderBy = field;
+    updateVariables.sortBy = order === "descend" ? "desc" : "asc";
+    setData({ variables: updateVariables });
+    getData();
+  }
 
   return (
     <MainLayout>
@@ -88,7 +153,15 @@ function Tags() {
         </Row>
         <Row>
           <Col span={18}>
-            <Table columns={columns} dataSource={data} />
+            <Table
+              columns={columns}
+              dataSource={data.tags}
+              pagination={{
+                pageSize: TABLE_LIMIT,
+                total: data.total,
+              }}
+              onChange={onChange}
+            />
             <TagModal
               isOpen={modalOpen}
               setIsOpen={setModalOpen}
