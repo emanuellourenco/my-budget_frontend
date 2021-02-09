58a70701a65838814b65474e45dd3c0dcdce1ca9
Emanuel Lourenço <dodgy6@gmail.com>
2021-02-09 12:42:44 +0000

    change auth component

--- src/views/Auth.jsx
+++ src/views/Auth.jsx
@@ -1,64 +1,68 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { Button, Row, Col } from "antd";
 import Login from "../components/auth/Login";
 import Signup from "../components/auth/Signup";
 
 function Auth() {
-  const [form, setForm] = useState("login");
   const history = useHistory();
+  const [form, setForm] = useState("login");
+  const [hideForm, setHideForm] = useState(false);
+  let buttonLabel = form === "login" ? "Signup" : "Login";
 
-  const changeForm = () => {
-    if (form === "login") {
-      setForm("signup");
-      history.push("/signup");
-    } else {
-      setForm("login");
-      history.push("/login");
-    }
+  const toggleForm = () => {
+    const action = form === "login" ? "signup" : "login";
+
+    setForm(action);
+    setHideForm(true);
+    history.push(`/${action}`);
   };
 
-  let loginClass = "auth auth__login";
-  let signupClass = "auth auth__signup";
-  let blockClass = "auth auth__block";
-  let buttonLabel;
+  /**
+   * Render Login or Signup form
+   */
+  const renderForm = () => {
+    return form === "login" ? <Login /> : <Signup />;
+  };
 
-  if (form === "login") {
-    signupClass += " hide--signup";
-    blockClass += " hide--signup";
-    buttonLabel = "Signup";
-  } else {
-    loginClass += " hide--login";
-    blockClass += " hide--login";
-    buttonLabel = "Login";
-  }
+  /**
+   * Show selected form after 1s
+   */
+  useEffect(() => {
+    if (hideForm) {
+      setTimeout(() => setHideForm(false), 1000);
+    }
+  }, [hideForm]);
 
   return (
     <div className="auth">
-      <Login className={loginClass} />
-      <div className={blockClass}>
-        <Row>
-          <Col span="24">
-            {form === "login" ? (
-              <>
-                <p>Don't have account?</p>
-                <p>Register Now!</p>
-              </>
-            ) : (
-              <>
-                <p>Already have account?</p>
-                <p>Login Now!</p>
-              </>
-            )}
-          </Col>
-        </Row>
-        <Row>
-          <Col span="24">
-            <Button onClick={() => changeForm()}>{buttonLabel}</Button>
-          </Col>
-        </Row>
+      <div className={`auth auth--form ${form === "login" ? "left" : "right"}`}>
+        {!hideForm && renderForm()}
       </div>
-      <Signup className={signupClass} />
+      <div className={`auth auth--info ${form === "login" ? "right" : "left"}`}>
+        <div className="info__block">
+          <Row>
+            <Col span="24">
+              {form === "login" ? (
+                <>
+                  <p>Don't have account?</p>
+                  <p>Register Now!</p>
+                </>
+              ) : (
+                <>
+                  <p>Already have account?</p>
+                  <p>Login Now!</p>
+                </>
+              )}
+            </Col>
+          </Row>
+          <Row>
+            <Col span="24">
+              <Button onClick={() => toggleForm()}>{buttonLabel}</Button>
+            </Col>
+          </Row>
+        </div>
+      </div>
     </div>
   );
 }
