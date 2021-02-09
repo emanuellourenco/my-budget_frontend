a69c38c595795254c758a2b65f9a79ad808c9710
Emanuel Lourenço <dodgy6@gmail.com>
2021-02-09 12:41:24 +0000

    remove unnecessary div in Login and Signup components

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -5,10 +5,9 @@ import Input from "../form/Input";
 import InputPassword from "../form/InputPassword";
 import axios from "axios";
 
-function Login(props) {
+function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const { className } = props;
   const url = process.env.REACT_APP_URL;
   const history = useHistory();
 
@@ -33,38 +32,36 @@ function Login(props) {
   };
 
   return (
-    <div className={className}>
-      <div className="login__block">
-        <Row justify="center">
-          <Col span="12">
-            <h1>Login</h1>
-            <Input
-              label="Email"
-              name="loginEmail"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
+    <div className="login__block">
+      <Row justify="center">
+        <Col span="12">
+          <h1>Login</h1>
+          <Input
+            label="Email"
+            name="loginEmail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
-            <InputPassword
-              label="Password"
-              name="loginPassword"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <Col span="24">
-              <Row justify="center">
-                <Button
-                  className="login__button"
-                  shape="round"
-                  onClick={() => submitForm()}
-                >
-                  Login
-                </Button>
-              </Row>
-            </Col>
+          <InputPassword
+            label="Password"
+            name="loginPassword"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Col span="24">
+            <Row justify="center">
+              <Button
+                className="login__button"
+                shape="round"
+                onClick={() => submitForm()}
+              >
+                Login
+              </Button>
+            </Row>
           </Col>
-        </Row>
-      </div>
+        </Col>
+      </Row>
     </div>
   );
 }
--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -1,34 +1,83 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { Button, Row, Col } from "antd";
 import Input from "../form/Input";
 import InputPassword from "../form/InputPassword";
+import axios from "axios";
 
-function Login(props) {
-  const { className } = props;
+function Login() {
+  const url = process.env.REACT_APP_URL;
+  const [user, setUser] = useReducer(
+    (state, newState) => ({ ...state, ...newState }),
+    {
+      name: "",
+      email: "",
+      password: "",
+      password2: "",
+    }
+  );
 
   const submitForm = () => {
-    alert("login fail!");
+    axios
+      .post(`${url}/signup`, user)
+      .then(({ data }) => {
+        console.log("🚀 ~ .then ~ data", data);
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      });
+  };
+
+  /**
+   * Change input data
+   * @param {Object} e
+   */
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ [name]: value });
   };
 
   return (
-    <div className={className}>
-      <div className="signup__block">
-        <Row justify="center">
-          <Col span="12">
-            <h1>Signup</h1>
-            <Input label="Name" name="name" />
-            <Input label="Email" name="email" />
+    <div className="signup__block">
+      <Row justify="center">
+        <Col span="12">
+          <h1>Signup</h1>
+          <Input
+            label="Name"
+            name="name"
+            value={user.name}
+            onChange={handleChange}
+          />
+          <Input
+            label="Email"
+            name="email"
+            value={user.email}
+            onChange={handleChange}
+          />
 
-            <InputPassword label="Password" name="password" />
-            <InputPassword label="Password" name="password2" />
-            <Col>
-              <Button className="signup__button" shape="round" onClick={() => submitForm()}>
-                Signup
-              </Button>
-            </Col>
+          <InputPassword
+            label="Password"
+            name="password"
+            value={user.password}
+            onChange={handleChange}
+          />
+          <InputPassword
+            label="Password"
+            name="password2"
+            value={user.password2}
+            onChange={handleChange}
+          />
+          <Col>
+            <Button
+              className="signup__button"
+              shape="round"
+              onClick={() => submitForm()}
+            >
+              Signup
+            </Button>
           </Col>
-        </Row>
-      </div>
+        </Col>
+      </Row>
     </div>
   );
 }
